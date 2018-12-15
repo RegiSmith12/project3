664cf9e4391d1a71ccaf2ef665312252e1d3ef41
RegiSmith12 <44275381+RegiSmith12@users.noreply.github.com>
2018-12-14 20:40:30 -0600

    Update project3.s
    read string and store the address

--- project3.s
+++ project3.s
@@ -1,5 +1,10 @@
 .data
-    emptyInput: .asciiz "Input is empty." #if string input is empty
-    longInput: .asciiz "Input it is too long." #if string has over 4 charcters
-    invalidInput: .asciiz "Invalid base-35 number." #if string has characters not in set
+    emptyString: .asciiz "Input is empty." #if string input is empty
+    longString: .asciiz "Input it is too long." #if string has over 4 charcters
+    invalidString: .asciiz "Invalid base-35 number." #if string has characters not in set
     userInput: .space 1000
+.text
+    main:
+        li $v0, 8 #read string
+        la $a0, userInput #store the address of string
+        li $a1, 500 #create enough space for string input
