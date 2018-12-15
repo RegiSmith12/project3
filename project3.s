f0bac5e578b7862c6eec9662f6d5f57d32322483
RegiSmith12 <44275381+RegiSmith12@users.noreply.github.com>
2018-12-14 20:41:54 -0600

    Update project3.s
    initialize t0 and t1 registers

--- project3.s
+++ project3.s
@@ -8,3 +8,9 @@
         li $v0, 8 #read string
         la $a0, userInput #store the address of string
         li $a1, 500 #create enough space for string input
+        syscall
+        add $t0, $0, 0 #initialize $t0 register
+        add $t1, $0, 0 #initialize $t1 register
+
+        la $t2, userInput #load string addr into $t2 register
+        lb $t0, 0($t2)
